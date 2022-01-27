ba3dac7b70df7758a045d3d2edfa20a4f5fa4f1c
Vlad Sizov <vladislav.sizov.2002@gmail.com>
2022-01-27 17:57:56 +0300

    1 point

--- visizovPW7/ViewController.swift
+++ visizovPW7/ViewController.swift
@@ -6,13 +6,51 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+final class MapController: UIViewController {
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        view.addSubview(mapView)
+        
+        configureUI()
+        
+
+
     }
+    
+    private let mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.layer.masksToBounds = true
+        mapView.layer.cornerRadius = 5
+        mapView.clipsToBounds = false
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.showsScale = true
+        mapView.showsCompass = true
+        mapView.showsTraffic = true
+        mapView.showsBuildings = true
+        mapView.showsUserLocation = true
+
+        
+        return mapView
+    } ()
+    
+    private func configureUI() {
+        mapView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        mapView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1).isActive = true
+        mapView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1).isActive = true
+        mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+     return .lightContent
+     }
+    
+    
 
 
 }
