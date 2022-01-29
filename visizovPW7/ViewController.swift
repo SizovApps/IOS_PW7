716df4b67acfa23787753059fd143c72a83e0e8a
Vlad Sizov <vladislav.sizov.2002@gmail.com>
2022-01-29 22:56:08 +0300

    2 point

--- visizovPW7/ViewController.swift
+++ visizovPW7/ViewController.swift
@@ -11,13 +11,85 @@ import MapKit
 
 final class MapController: UIViewController {
 
+    let goButton: UIButton = {
+        let goButton = UIButton(type: .system)
+        goButton.setTitle("Go", for: .normal)
+        goButton.translatesAutoresizingMaskIntoConstraints = false
+        goButton.backgroundColor = .blue
+        return goButton
+    }()
+    
+    let clearButton: UIButton = {
+        let clearButton = UIButton(type: .system)
+        clearButton.setTitle("Clear", for: .normal)
+        clearButton.translatesAutoresizingMaskIntoConstraints = false
+        clearButton.backgroundColor = .gray
+        return clearButton
+    }()
+    
+    let startLocation: UITextField = {
+     let control = UITextField()
+     control.backgroundColor = UIColor.lightGray
+     control.textColor = UIColor.black
+     control.placeholder = "From"
+     control.layer.cornerRadius = 2
+     control.clipsToBounds = false
+     control.font = UIFont.systemFont(ofSize: 15)
+     control.borderStyle = UITextField.BorderStyle.roundedRect
+     control.autocorrectionType = UITextAutocorrectionType.yes
+     control.keyboardType = UIKeyboardType.default
+     control.returnKeyType = UIReturnKeyType.done
+     control.clearButtonMode =
+    UITextField.ViewMode.whileEditing
+     control.contentVerticalAlignment =
+    UIControl.ContentVerticalAlignment.center
+     return control
+     }()
+    
+    let endLocation: UITextField = {
+     let control = UITextField()
+     control.backgroundColor = UIColor.lightGray
+     control.textColor = UIColor.black
+     control.placeholder = "End"
+     control.layer.cornerRadius = 2
+     control.clipsToBounds = false
+     control.font = UIFont.systemFont(ofSize: 15)
+     control.borderStyle = UITextField.BorderStyle.roundedRect
+     control.autocorrectionType = UITextAutocorrectionType.yes
+     control.keyboardType = UIKeyboardType.default
+     control.returnKeyType = UIReturnKeyType.done
+     control.clearButtonMode =
+    UITextField.ViewMode.whileEditing
+     control.contentVerticalAlignment =
+    UIControl.ContentVerticalAlignment.center
+     return control
+     }()
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
     
         view.addSubview(mapView)
         
+        view.addSubview(goButton)
+        view.addSubview(clearButton)
+        
         configureUI()
+        
+        SetupButtons()
+        
+        let textStack = UIStackView()
+         textStack.axis = .vertical
+         view.addSubview(textStack)
+         textStack.spacing = 10
+         textStack.pin(to: view, [.top: 50, .left: 10, .right: 10])
+         [startLocation, endLocation].forEach { textField in
+         textField.setHeight(to: 40)
+         textField.delegate = self
+         textStack.addArrangedSubview(textField)
+         }
         
 
 
@@ -50,8 +122,29 @@ final class MapController: UIViewController {
      return .lightContent
      }
     
+    private func SetupButtons() {
+        goButton.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        goButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/2).isActive = true
+        goButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+        
+        clearButton.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        clearButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/2).isActive = true
+        clearButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+        
     
+    }
+    
+    @objc func clearButtonWasPressed() {
+    
+    }
+
+}
 
 
+extension MapController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
